d1abb581e2b345bb8c04c5fdf0b16a03344d910c
Zuzeth Romo <zuzethromo@yahoo.com>
2019-06-05 16:22:59 -0600

    added images on page random number generator and css

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+let win;
+let loss;
+
+
+for(let i = 0; i < 4; i++){
+let crystalNumber = Math.floor(Math.random() * 11) + 1;
+// console.log(crystalNumber);
+}
+
+let randomNumber = Math.floor(Math.random() * 101) + 19;
+// console.log(randomNumber);
+$(".numberRandom").html('Reach this Number: ' + randomNumber);
