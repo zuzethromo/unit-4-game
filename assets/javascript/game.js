7d3356bd76fd6e3c2837fd600e2f16dd41ffbb81
Zuzeth Romo <zuzethromo@yahoo.com>
2019-06-06 16:33:11 -0600

    added reset and total score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,44 +2,49 @@ let win = 0;
 let loss = 0;
 let totalScore = 0;
 
-// let startGame = function(){}
+let startGame = function () {
 
-let randomNumber = Math.floor(Math.random() * 101) + 19;
+    // $(".crystals").empty();
+
+    randomNumber = Math.floor(Math.random() * 101) + 19;
 // console.log(randomNumber);
 $(".numberRandom").html('Reach this Number: ' + randomNumber);
 
+for(let i = 0; i < 4; i++){
 
-// for(let i = 0; i < 4; i++){}
-
-
-let crystalNumber = Math.floor(Math.random() * 11) + 1;
+ crystalNumber = Math.floor(Math.random() * 11) + 1;
 // console.log(crystalNumber);
 
-// startGame();
+}
+}
 
+startGame();
 
 $(".crystals").on('click', function() {
     // console.log($(this));
 let number = totalScore + crystalNumber;
   totalScore += number;
-  console.log(totalScore);
+//   console.log(totalScore);
   
 if(totalScore === randomNumber){
      win++;
-    //  startGame();
-    console.log("win");
+     totalScore = 0;
+     startGame();
+    // console.log("win");
 }
 else if(totalScore > randomNumber){
     loss++;
-    // startGame();
-    console.log("loss");
+    totalScore = 0;
+    startGame();
+    // console.log("loss");
 }
 
 let html = 
-"<h3> Wins: " + win + "</h3>" +
-"<h3> Losses: " + loss + "</h3>"
+"<h3> Total Score: " + totalScore + "</h3>" +
+"<h4> Wins: " + win + "</h4>" +
+"<h4> Losses: " + loss + "</h4>"
 
-document.querySelector('#count').innerHTML = html;
+document.querySelector('.count').innerHTML = html;
 });
 
 
